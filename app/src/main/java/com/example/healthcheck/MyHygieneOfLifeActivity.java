0902b42c72402672dca4e44843ea8be54aeb9848
Hylionx <39769974+hylionx@users.noreply.github.com>
2022-02-24 21:16:34 +0100

    Add all activities with questions string

--- app/src/main/java/com/example/healthcheck/MyHygieneOfLifeActivity.java
+++ app/src/main/java/com/example/healthcheck/MyHygieneOfLifeActivity.java
@@ -3,12 +3,17 @@ package com.example.healthcheck;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.widget.SwitchCompat;
+
 public class MyHygieneOfLifeActivity extends BaseActivity {
     public static final String APP_TAG = "MyHygieneOfLifeActivityApp";
 
     Button btnPrevMyHygieneOfLife;
     Button btnValidate;
-
+    SwitchCompat switchMyHygieneOfLifeQ1;
+    SwitchCompat switchMyHygieneOfLifeQ2;
+    SwitchCompat switchMyHygieneOfLifeQ3;
+    SwitchCompat switchMyHygieneOfLifeQ4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +21,50 @@ public class MyHygieneOfLifeActivity extends BaseActivity {
         setContentView(R.layout.activity_my_hygiene_of_life);
 
         btnPrevMyHygieneOfLife = findViewById(R.id.btnPrevMyHygieneOfLife);
+        btnValidate = findViewById(R.id.btnValidate);
+
+        switchMyHygieneOfLifeQ1 = findViewById(R.id.switchMyHygieneOfLifeQ1);
+        switchMyHygieneOfLifeQ2 = findViewById(R.id.switchMyHygieneOfLifeQ2);
+        switchMyHygieneOfLifeQ3 = findViewById(R.id.switchMyHygieneOfLifeQ3);
+        switchMyHygieneOfLifeQ4 = findViewById(R.id.switchMyHygieneOfLifeQ4);
 
         btnPrevMyHygieneOfLife.setOnClickListener(view -> gotoPreviousActivity());
 
         getPersonByIntent();
-        questions = new String[]{"f7_q1", "f7_q2", "f7_q3", "f7_q4"};
+        questions = new String[]{"f8_q1", "f8_q2", "f8_q3", "f8_q4"};
 
     }
 
     @Override
     protected boolean validateWidgetsAndAffectPersonDatas() {
+
+
+        if(switchMyHygieneOfLifeQ1.isChecked()){
+            person.addQA(questions[0], getString(R.string.txtMyHygieneOfLifeQ1), 1, getString(R.string.txt_Yes));
+        } else {
+            person.addQA(questions[0], getString(R.string.txtMyHygieneOfLifeQ1), 0, getString(R.string.txt_No));
+        }
+
+        if(switchMyHygieneOfLifeQ2.isChecked()){
+            person.addQA(questions[1], getString(R.string.txtMyHygieneOfLifeQ2), 1, getString(R.string.txt_Yes));
+        } else {
+            person.addQA(questions[1], getString(R.string.txtMyHygieneOfLifeQ2), 0, getString(R.string.txt_No));
+        }
+
+
+        if(switchMyHygieneOfLifeQ3.isChecked()){
+            person.addQA(questions[2], getString(R.string.txtMyHygieneOfLifeQ3), 1, getString(R.string.txt_Yes));
+        } else {
+            person.addQA(questions[2], getString(R.string.txtMyHygieneOfLifeQ3), 0, getString(R.string.txt_No));
+        }
+
+        if(switchMyHygieneOfLifeQ4.isChecked()){
+            person.addQA(questions[3], getString(R.string.txtMyHygieneOfLifeQ4), 1, getString(R.string.txt_Yes));
+        } else {
+            person.addQA(questions[3], getString(R.string.txtMyHygieneOfLifeQ4), 0, getString(R.string.txt_No));
+        }
+
+
         return true;
     }
 }
--- app/src/main/java/com/example/healthcheck/MyStressManagementActivity.java
+++ app/src/main/java/com/example/healthcheck/MyStressManagementActivity.java
@@ -2,12 +2,18 @@ package com.example.healthcheck;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Spinner;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class MyStressManagementActivity extends BaseActivity {
     public static final String APP_TAG = "MyStressManagementActivityApp";
 
     Button btnPrevMyStressManagment;
     Button btnNextMyStressManagment;
+    Spinner spinnerMyStressManagementRepQ1;
+    Spinner spinnerMyStressManagementRepQ2;
+    SwitchCompat switchMyStressManagementQ3;
+    SwitchCompat switchMyStressManagementQ4;
 
 
     @Override
@@ -18,15 +24,43 @@ public class MyStressManagementActivity extends BaseActivity {
         btnPrevMyStressManagment = findViewById(R.id.btnPrevMyStressManagment);
         btnNextMyStressManagment = findViewById(R.id.btnNextMyStressManagment);
 
+        spinnerMyStressManagementRepQ1 = findViewById(R.id.spinnerMyStressManagementRepQ1);
+        spinnerMyStressManagementRepQ2 = findViewById(R.id.spinnerMyStressManagementRepQ2);
+        switchMyStressManagementQ3 = findViewById(R.id.switchMyStressManagementQ3);
+        switchMyStressManagementQ4 = findViewById(R.id.switchMyStressManagementQ4);
+
+
         btnPrevMyStressManagment.setOnClickListener(view -> gotoPreviousActivity());
         btnNextMyStressManagment.setOnClickListener(view -> gotoNextActivity(MyHygieneOfLifeActivity.class));
 
         getPersonByIntent();
-        questions = new String[]{"f6_q1", "f6_q2", "f6_q3"};
+        questions = new String[]{"f7_q1", "f7_q2", "f7_q3", "f7_q4"};
     }
 
     @Override
     protected boolean validateWidgetsAndAffectPersonDatas() {
+        int posq1 = spinnerMyStressManagementRepQ1.getSelectedItemPosition();
+        int posq2 = spinnerMyStressManagementRepQ2.getSelectedItemPosition();
+
+        String q1 =  spinnerMyStressManagementRepQ1.getSelectedItem().toString();
+        String q2 = spinnerMyStressManagementRepQ2.getSelectedItem().toString();
+
+        person.addQA(questions[0], getString(R.string.txtMyStressManagementQ1), posq1, q1);
+        person.addQA(questions[1], getString(R.string.txtMyStressManagementQ2), posq2, q2);
+
+        if(switchMyStressManagementQ3.isChecked()){
+            person.addQA(questions[2], getString(R.string.txtMyStressManagementQ3), 1, getString(R.string.txt_Yes));
+        } else {
+            person.addQA(questions[2], getString(R.string.txtMyStressManagementQ3), 0, getString(R.string.txt_No));
+        }
+
+        if(switchMyStressManagementQ4.isChecked()){
+            person.addQA(questions[3], getString(R.string.txtMyStressManagementQ4), 1, getString(R.string.txt_Yes));
+        } else {
+            person.addQA(questions[3], getString(R.string.txtMyStressManagementQ4), 0, getString(R.string.txt_No));
+        }
+
+
         return true;
     }
 }
